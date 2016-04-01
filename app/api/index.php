82d786e813379f66382eb930bf6423feb1cefd6b
willsky <xuxianhua1985@gmail.com>
2016-04-02 01:22:15 +0800

    注册自定义shutdown

--- app/api/index.php
+++ app/api/index.php
@@ -15,4 +15,17 @@ require(ROOT_PATH . DS . 'vendor' . DS . 'autoload.php');
         echo json_encode(array('c' => $code, 'msg' => $message));
         exit;
     });
+    
+    \Quick\Server::registerShutdown(function() {
+        if (function_exists('app_shutdown')) {
+            app_shutdown();
+        }
+        
+        if (($error = error_get_last()) && in_array($error['type'], array(E_ERROR, E_PARSE, E_CORE_ERROR,E_COMPILE_ERROR, E_CORE_WARNING, E_COMPILE_WARNING))) {
+            header('Content-Type: application/json; charset='.\Quick\Core\App::instance()->get('charset'));
+            $code = ERROR_SYSTEM_CORE;
+            $message = $error['message'];
+            echo json_encode(array('c' => $code, 'msg' => $message));
+        }
+    });
 });
